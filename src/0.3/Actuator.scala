f965a2ce9618667dfa9e2780d145e59046e82d8a
Comcx <comcx@outlook.com>
2018-09-13 08:43:51 -0500

    Add files via upload

---
+++ src/0.3/Actuator.scala
@@ -0,0 +1,40 @@
+package actuator
+
+import java.util.Scanner
+import parser._
+import scanner._
+
+
+class Actuator() {
+
+  def repl(): Unit = {
+
+    val p = new Parser()
+    val r = new Reader()
+    val s = new Scanner(System.in)
+
+    var go_on = true
+    while (go_on) {
+
+      print("\nnano> ")
+      val input = Console.readLine()
+      if (input == ":q") go_on = false
+      else if (input == ":load") ???
+      else {
+        val res = p.parse(r.read(input))
+        println("-> " + res)
+      }
+    }// end while
+  }
+
+
+}
+
+
+
+
+
+
+
+
+
